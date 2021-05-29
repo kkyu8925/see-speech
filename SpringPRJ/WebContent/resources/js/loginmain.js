26a6c334fcdb6e05baac0e09cc9f68ca8ea4e66b
kkyu <gusrb8925@naver.com>
2021-05-29 18:42:10 +0900

    21-05-29 update loginmain.js validate

--- SpringPRJ/WebContent/resources/js/loginmain.js
+++ SpringPRJ/WebContent/resources/js/loginmain.js
@@ -1,32 +1,30 @@
-
 (function ($) {
     "use strict";
 
     /*==================================================================
     [ Validate after type ]*/
-    $('.validate-input .input100').each(function(){
-        $(this).on('blur', function(){
-            if(validate(this) == false){
+    $('.validate-input .input100').each(function () {
+        $(this).on('blur', function () {
+            if (validate(this) === false) {
                 showValidate(this);
-            }
-            else {
+            } else {
                 $(this).parent().addClass('true-validate');
             }
-        })    
+        })
     })
-  
-  
+
+
     /*==================================================================
     [ Validate ]*/
     var input = $('.validate-input .input100');
 
-    $('.validate-form').on('submit',function(){
+    $('.validate-form').on('submit', function () {
         var check = true;
 
-        for(var i=0; i<input.length; i++) {
-            if(validate(input[i]) == false){
+        for (var i = 0; i < input.length; i++) {
+            if (validate(input[i]) === false) {
                 showValidate(input[i]);
-                check=false;
+                check = false;
             }
         }
 
@@ -34,21 +32,24 @@
     });
 
 
-    $('.validate-form .input100').each(function(){
-        $(this).focus(function(){
-           hideValidate(this);
-           $(this).parent().removeClass('true-validate');
+    $('.validate-form .input100').each(function () {
+        $(this).focus(function () {
+            hideValidate(this);
+            $(this).parent().removeClass('true-validate');
         });
     });
 
-     function validate (input) {
-        if($(input).attr('type') == 'email' || $(input).attr('name') == 'email') {
-            if($(input).val().trim().match(/^([a-zA-Z0-9_\-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([a-zA-Z0-9\-]+\.)+))([a-zA-Z]{1,5}|[0-9]{1,3})(\]?)$/) == null) {
+    function validate(input) {
+        if ($(input).attr('type') === 'email' || $(input).attr('name') === 'email') {
+            if ($(input).val().trim().match(/^([a-zA-Z0-9_\-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([a-zA-Z0-9\-]+\.)+))([a-zA-Z]{1,5}|[0-9]{1,3})(\]?)$/) == null) {
+                return false;
+            } else if (input.parentNode.classList.contains("exist_user")) {
                 return false;
             }
-        }
-        else {
-            if($(input).val().trim() == ''){
+        } else if ($(input).attr('name') === 'user_pw_check' && $("#user_pw_check").val() !== $("#user_pw").val()) {
+            return false;
+        } else {
+            if ($(input).val().trim() === '') {
                 return false;
             }
         }
@@ -60,9 +61,9 @@
         $(thisAlert).addClass('alert-validate');
 
         $(thisAlert).append('<span class="btn-hide-validate">&#xf135;</span>')
-        $('.btn-hide-validate').each(function(){
-            $(this).on('click',function(){
-               hideValidate(this);
+        $('.btn-hide-validate').each(function () {
+            $(this).on('click', function () {
+                hideValidate(this);
             });
         });
     }
@@ -72,7 +73,6 @@
         $(thisAlert).removeClass('alert-validate');
         $(thisAlert).find('.btn-hide-validate').remove();
     }
-    
-    
+
 
 })(jQuery);
